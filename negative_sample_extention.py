10fdc9c6b0a2276bb67796b0db7423e79b72200c
Qiming Bao <qbao775@aucklanduni.ac.nz>
2022-11-10 09:30:32 +0000

    same size of sample

--- negative_sample_extention.py
+++ negative_sample_extention.py
@@ -3,11 +3,14 @@ filename = "Synthetic_xfm_t5wtense_logical_equivalence_train_v4"
 dataframe = pd.read_csv("./output_result/"+filename+".csv")
 df = pd.DataFrame(columns=['sentence1', 'sentence2', 'label'])
 import random
+from sklearn.utils import shuffle
 
 negative_dataframe = dataframe.loc[dataframe['label'] == 0]
 negative_dataframe.reset_index(drop=True, inplace=True)
 
-negative_sample_k = 2
+cut_line = dataframe.shape[0]
+
+negative_sample_k = 2  ## add 50% negative samples
 for index, row in dataframe.iterrows():
     df = df.append(
         {'sentence1': row["sentence1"], 'sentence2': row["sentence2"],
@@ -22,5 +25,7 @@ for index, row in dataframe.iterrows():
                 'label': 0},
                 ignore_index=True)
 
+df = shuffle(df)
+df_final = df.head(cut_line)
 
-df.to_csv("./output_result/"+filename+"_negative_samples_1_"+str(negative_sample_k+1)+".csv",index = None,encoding = 'utf8')
+df_final.to_csv("./output_result/"+filename+"_negative_samples_1_"+str(negative_sample_k+1)+".csv",index = None,encoding = 'utf8')
